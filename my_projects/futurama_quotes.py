7fbbda9b884ce5a03bd6a06d0c0d1b169875ada2
hussainkghani <hussain.k.ghani@outlook.com>
2023-02-23 17:40:07 +0000

    fixed the random quotes generating

--- my_projects/futurama_quotes.py
+++ my_projects/futurama_quotes.py
@@ -7,11 +7,15 @@ import random
 def main():
     #runtime function
     quotes = {
-            "Fry": ["Shut up and take my money!", "Wait I'm having one of those things, you know, a headache with pictures", "I'm never gonna get used to the 31st century. Caffeinated bacon? Baconated grapefruit? Admiral Crunch?"],
-            "Bender": ["I hope he didn't die. Unless he l;eft a notes naming me his sucessor, then I hope he did die", 
+            "Fry": ["Shut up and take my money!", "Wait I'm having one of those things, you know, a headache with pictures",
+            "I'm never gonna get used to the 31st century. Caffeinated bacon? Baconated grapefruit? Admiral Crunch?"],
+            "Bender": ["I hope he didn't die. Unless he left a notes naming me his sucessor, then I hope he did die", 
                 "Bite my shiny metal ass", "I'm so embarrased. I wish everybody else was dead"],
-            "Zapp": ["If we hit that bullseye, the rest of the dominos will fall like a house of cards. Checkmate", "When I'm in command, ever mission is a suicide mission", "She's built like a steakhouse, but she handles like a Bistro"],
-            "Prof Farnsworth": ["God didn't get to be God by giving away money!", "I was born in prison and I'll die in prison.", "I'm sciencing as fast as I can."]
+            "Zapp": ["If we hit that bullseye, the rest of the dominos will fall like a house of cards. Checkmate",
+                "When I'm in command, ever mission is a suicide mission",
+                "She's built like a steakhouse, but she handles like a Bistro"],   
+            "Prof Farnsworth": ["God didn't get to be God by giving away money!", "I was born in prison and I'll die in prison.",
+                "I'm sciencing as fast as I can."]
             }
 
 #Step 2: ask the user who their favorite futurama character is
@@ -35,6 +39,8 @@ def main():
             print("There are no quotes for this character please type in 'Fry', ''Bender', 'Zapp', or 'Prof Farnsworth")
 
 #have validation for user inputs
+
+#maybe add episode where the quote appears 
 
 #call the main function
 if __name__ == "__main__":
