6ba79e6bbba03ea1e4d496937cfa1ff273874182
hussainkghani <hussain.k.ghani@outlook.com>
2023-02-28 21:23:37 +0000

    learning about YAML

---
+++ my_projects/megaman_rpg.py
@@ -0,0 +1,23 @@
+# Create a simple RPG game for the video game character Megaman.exe
+
+# Display a start Menu to users with Title, start game, close game
+
+# Once megaman is logged into the net he will start in ACDC Area
+
+# Areas will be ACDC, SciLab, Oran_Island, Undernet, Secret_Area
+
+# Megaman will battle viruses in each area in order to earn bug frags
+
+# He will have option to skip the areas without bosses to finish the game, but he will not be able to access the secret area and face the secret boss without clearing each room
+
+# Megaman will be given a folder of battlechips to be used 
+
+# turn based combat system where the player will attack first followed by an enemy attack 
+
+# Megaman will have a starting HP of 1000 and the player will have the option to bypass rooms without a boss
+
+# Battle Chip Library will include recovery chips so that the player doesn't have to restart
+
+# Boss Battle will be Dark_Megaman 
+
+# Secret area battle will be Bass after beating every virus/boss and collecting the bug frags
